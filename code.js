92d051f9256d8bdce3ef44f8eb0d38ff60e7b4c4
jose francisco sanchez <100879436+panchocode@users.noreply.github.com>
2022-05-12 11:07:06 -0500

    menu ham listo

--- code.js
+++ code.js
@@ -1,15 +1,7 @@
 const menu = document.querySelector(".menu-ico");
 const lista = document.querySelector(".lista");
-const x = document.querySelector(".x-ico");
+
 //mostrar menu
 menu.addEventListener("click", () => {
   lista.classList.toggle("mostrar-lista");
-  x.classList.toggle("mostrar");
-  menu.classList.toggle("ocultar");
-});
-
-x.addEventListener("click", () => {
-  menu.classList.toggle("mostrar");
-  lista.classList.toggle("ocultar");
-  x.classList.toggle("ocultar");
 });
